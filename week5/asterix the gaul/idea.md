73e447ca4a7046e6175d761fdd809b1c8761a2c3
Zilin Wang <flwzxy@gmail.com>
2021-02-03 17:13:17 +0100

    "review gaul."

--- week5/asterix the gaul/idea.md
+++ week5/asterix the gaul/idea.md
@@ -1,3 +1,9 @@
 # Idea
 
 The idea is to binary search the potions needed. In the search, once the amount of the potion is fixed, the problem is reduced to a backpack problem. However, the time complexity will be D*T which is too high for using DP. On that end we use the split technique, to reduce the time complexity for a constant coefficient.
+
+# Remark
+
+It's worth noting that when using for (auto t : v) to iterate over a vector, the variable t is always a copy of an element in v. If we want to modify v. We have to explicitly get the reference of that element using &.
+
+It's also worth noting that whenever using a sliding window / double pointers approach, always try to make the sequence monotonic. In the case of this task, remember to postprocess the tot_time and tot_dist of all possible subsets after sorting by time. We need to keep that the distance in the latter element (which indicates its time will be larger than the former ones) is always larger than that of the former ones.
--- week5/asterix the gaul/src/review_backup.cpp
+++ week5/asterix the gaul/src/review_backup.cpp
@@ -1,61 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 #include <algorithm>
 
 using namespace std;
 
-typedef vector<pair<long, long> > VP;
-
 bool compare (pair<long, long> p1, pair<long, long> p2) {
     if (p1.second == p2.second) {
         return p1.first > p2.first;
-    } 
+    }
     return p1.second < p2.second;
 }
 
-bool feasible(long inc, long D, long T, VP movements) {
-    for (auto& p : movements) {
+bool feasible(long inc, long D, long T, vector<pair<long, long> > movements) {
+    for (auto &p : movements) {
         p.first += inc;
     }
+
     int m = movements.size();
-    VP S1 = VP(movements.begin(), movements.begin() + m / 2);
-    VP S2 = VP(movements.begin() + m / 2, movements.end());
-    int n1 = S1.size();
-    int n2 = S2.size();
+    vector<pair<long, long> > S1 = vector<pair<long, long> > (movements.begin(), movements.begin() + m / 2);
+    vector<pair<long, long> > S2 = vector<pair<long, long> > (movements.begin() + m / 2, movements.end());
 
-    // cout << "size: " << S1.size() << " " << S2.size() << endl;
-
-    VP sub_S1, sub_S2;
-    for (int s = 0; s < 1<<n1; ++s) { // Iterate through all subsets
+    vector<pair<long, long> > sub_S1, sub_S2;
+    for (int s = 0; s < 1<<S1.size(); ++s) { // Iterate through all subsets
         long tot_D = 0, tot_T = 0;
-        for (int i = 0; i < n1; ++i) {
+        for (int i = 0; i < S1.size(); ++i) {
+            // If i-th element in subset
             if (s & 1<<i) {
                 tot_D += S1[i].first;
                 tot_T += S1[i].second;
-            }
+            } 
         }
         sub_S1.push_back(make_pair(tot_D, tot_T));
     }
-
-    for (int s = 0; s < 1<<n2; ++s) { // Iterate through all subsets
+    for (int s = 0; s < 1<<S2.size(); ++s) { // Iterate through all subsets
         long tot_D = 0, tot_T = 0;
-        for (int i = 0; i < n2; ++i) {
+        for (int i = 0; i < S2.size(); ++i) {
+            // If i-th element in subset
             if (s & 1<<i) {
                 tot_D += S2[i].first;
                 tot_T += S2[i].second;
-            }
+            } 
         }
         sub_S2.push_back(make_pair(tot_D, tot_T));
     }
 
-    // cout << "sub size: " << sub_S1.size() << " " << sub_S2.size() << endl;
-
     sort(sub_S1.begin(), sub_S1.end(), compare);
     sort(sub_S2.begin(), sub_S2.end(), compare);
 
-    long max_d = 0;
-    for (auto& p : sub_S1) {
+    long max_d = 0; 
+    for (auto & p : sub_S1) {
         if (max_d > p.first) {
             p.first = max_d;
         } else {
@@ -63,32 +56,33 @@ bool feasible(long inc, long D, long T, VP movements) {
         }
     }
 
-    max_d = 0;
-    for (auto& p : sub_S2) {
+    max_d = 0; 
+    for (auto & p : sub_S2) {
         if (max_d > p.first) {
             p.first = max_d;
         } else {
             max_d = p.first;
         }
     }
+
 
     bool found = false;
-    int ind1 = 0, ind2 = sub_S2.size() - 1;
-    while (ind1 < sub_S1.size() && ind2 >= 0) {
-        long tot_D = sub_S1[ind1].first + sub_S2[ind2].first;
-        long tot_T = sub_S1[ind1].second + sub_S2[ind2].second;
-        // cout << ind1 << " " << ind2 << " " << tot_D << " " << tot_T << endl;
+    int ptr1 = 0, ptr2 = sub_S2.size() - 1;
+    // cout << "! ";
+    while (ptr1 < sub_S1.size() && ptr2 >= 0) {
+        long tot_D = sub_S1[ptr1].first + sub_S2[ptr2].first;
+        long tot_T = sub_S1[ptr1].second + sub_S2[ptr2].second;
+        // cout << ptr1 << " " << ptr2 << " " << tot_D << " " << tot_T << endl;
         if (tot_D >= D) {
-            ind2--;
+            ptr2--;
             if (tot_T < T) {
                 found = true;
                 break;
             }
         } else {
-            ind1++;
+            ptr1++;
         }
     }
-
     return found;
 }
 
@@ -96,30 +90,29 @@ void testcase() {
     int n, m;
     long D, T;
     cin >> n >> m >> D >> T;
-    VP movements;
+    vector<pair<long, long> > movements;
     for (int i = 0; i < n; i++) {
         long d, t; cin >> d >> t;
         movements.push_back(make_pair(d, t));
     }
-    vector<long> increments;
-    increments.push_back(0);
+    vector<long> reduction;
+    reduction.push_back(0);
     for (int i = 0; i < m; i++) {
         long s; cin >> s;
-        increments.push_back(s);
+        reduction.push_back(s);
     }
 
     int l = 0, r = m;
     while (l < r) {
         int mid = (l + r) / 2;
-        long inc = increments[mid];
-        if (feasible(inc, D, T, movements)) {
+        if (feasible(reduction[mid], D, T, movements)) {
             r = mid;
-        } else{
+        } else {
             l = mid + 1;
         }
     }
 
-    if (feasible(increments[l], D, T, movements)) {
+    if (feasible(reduction[l], D, T, movements)) {
         cout << l << endl;
     } else {
         cout << "Panoramix captured" << endl;
@@ -129,7 +122,7 @@ void testcase() {
 int main() {
     ios_base::sync_with_stdio(false);
     int t; cin >> t;
-    while (t--) {
+    while(t--) {
         testcase();
     }
 }
